38611e0772076c82731233bfdd65da75fd34fd96
akkdev1 <akkaradechsr@gmail.com>
2023-01-22 01:28:03 +0700

    Reg Form OK

--- src/Register.js
+++ src/Register.js
@@ -71,7 +71,7 @@ const Register = () => {
             setErrMsg("Invalid Entry");
             return;
         }
-        console.log(user.pwd)
+        console.log(user, pwd)
         setSuccess(true)
 
 
